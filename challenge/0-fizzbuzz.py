3ce5ae649248fe2279c2783e63775593112fc786
Nada-Al-Shuraidah <10718@holbertonstudents.com>
2025-09-28 16:03:02 +0000

    Update

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -33,3 +33,7 @@ if __name__ == '__main__':
         print("Missing number")
         print("Usage: ./0-fizzbuzz.py <number>")
         print("Example: ./0-fizzbuzz.py 89")
+        sys.exit(1)
+
+    number = int(sys.argv[1])
+    fizzbuzz(number)
