cf41da658ffe244b964764d73dc2cfec9428b484
Nada-Al-Shuraidah <10718@holbertonstudents.com>
2025-09-28 18:51:48 +0300

    Update 1-print_square.js

--- challenge/1-print_square.js
+++ challenge/1-print_square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /*
     Print a square with the character #
-    
+
     The size of the square must be the first argument 
     of the program.
 */
@@ -16,9 +16,9 @@ if (process.argv.length <= 2) {
 
 size = parseInt(process.argv[2], 10)
 
-for (let i = 0 ; i < size ; i ++){
-    {
-    for (let j = 0 ; j < size ; j ++)
+for (let i = 0 ; i < size ; i ++) {
+    for (let j = 0 ; j < size ; j ++) {
         process.stdout.write("#");
-        }
-    process.stdout.write("\n");}
+    }
+    process.stdout.write("\n");
+}
